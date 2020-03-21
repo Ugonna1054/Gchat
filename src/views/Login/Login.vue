a3e034a25639bf44a8b3edd88e705be02a640865
Ebenezer Ndukwe <ebnettech@gmail.com>
2020-03-21 16:19:17 +0100

    LoginUser Page added

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -3,9 +3,13 @@
         <div></div>
         <div><img src="../../assets/images/gchat-logo2.svg"></div>
         <div class="chat-with">Chat with friend in every school, <br>share moments, share contents.</div>
+        <div class="login-click mb-3">
+            <font-awesome-icon icon="user" class="search"/>
+            <router-link to="Verify"><button class="login-btn">Sign up with Email</button></router-link>
+        </div>
         <div class="login-click">
-            <img src="../../assets/images/call.svg">
-            <router-link to="Verify"><button class="login-btn">Login with Email</button></router-link>
+            <font-awesome-icon icon="user" class="search"/>
+            <router-link to="LoginUser"><button class="login-btn">Login with Email</button></router-link>
         </div>
         <div></div>
     </div>
